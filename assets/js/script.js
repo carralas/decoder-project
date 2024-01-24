413902d03ff45a59d1ed75a40cba9b96839d380a
lucas! <carralas@icloud.com>
2024-01-24 17:31:41 -0300

    style: add semi-colons to js script

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,11 @@ view = {
     copyButton: document.querySelector('.copy'),
     outputBoxEmpty: document.querySelector('.output__empty'),
     outputBoxFilled: document.querySelector('.output__filled')
-}
+};
 
 value = {
     inputFirstClick: false
-}
+};
 
 code = {
     encrypt: {'a': 'AI',
@@ -23,38 +23,38 @@ code = {
               'imes': 'I',
               'ober': 'O',
               'ufat': 'U'}
-}
+};
 
 function translator(input, model) {
-    let output = input
+    let output = input;
     /* o looping é feito por chave */
     /* isso faz com que só ocorra cinco vezes */
     /* evita que a etapa de encriptação passe em uma posição com caracter recém criado */
     /* facilita a etapa de desencriptação */
-    Object.keys(model).forEach(key => output = output.replaceAll(key, model[key]))
-    view.outputBoxEmpty.style.display = 'none'
-    view.outputBoxFilled.style.display = 'flex'
-    view.outputText.innerHTML = output.toLowerCase()
+    Object.keys(model).forEach(key => output = output.replaceAll(key, model[key]));
+    view.outputBoxEmpty.style.display = 'none';
+    view.outputBoxFilled.style.display = 'flex';
+    view.outputText.innerHTML = output.toLowerCase();
 }
 
 function inputCleaner() {
-    console.log('clicou')
+    console.log('clicou');
     if (value.inputFirstClick == false) {
-        console.log('limpou')
-        view.inputText.innerHTML = ''
-        value.inputFirstClick = true
+        console.log('limpou');
+        view.inputText.innerHTML = '';
+        value.inputFirstClick = true;
     }
 }
 
 function copyTranslation() {
-    navigator.clipboard.writeText(view.outputText.innerHTML)
+    navigator.clipboard.writeText(view.outputText.innerHTML);
 }
 
 function main() {
-    view.encodeButton.addEventListener('click', () => translator(view.inputText.value, code.encrypt))
-    view.decodeButton.addEventListener('click', () => translator(view.inputText.value, code.decrypt))
-    view.inputText.addEventListener('click', () => inputCleaner())
-    view.copyButton.addEventListener('click', () => copyTranslation())
+    view.encodeButton.addEventListener('click', () => translator(view.inputText.value, code.encrypt));
+    view.decodeButton.addEventListener('click', () => translator(view.inputText.value, code.decrypt));
+    view.inputText.addEventListener('click', () => inputCleaner());
+    view.copyButton.addEventListener('click', () => copyTranslation());
 }
 
 main();
